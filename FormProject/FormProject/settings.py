99dc9742551f1708f1f92fc5c2c86864f5b580d1
Rhema7776 <rhemachukwumah7@gmail.com>
2022-10-08 09:52:24 +0100

    latest commit2

--- FormProject/FormProject/settings.py
+++ FormProject/FormProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7s%7t8!c6!*rlo)vfv^94_5kq@icwj0f**anhdsbv3fdqt29fq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://form-authd.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATIC_URL = 'static/'
+
+SATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
